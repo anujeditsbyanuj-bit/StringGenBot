d9e2200c61042f0c060cfc239ea583e7e2acc7e4
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-07-31 13:05:08 +0530

    Update config.py
    Signed-off-by: anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv("BOT_TOKEN", "8730834688:AAEhm3QUYi93sBAMJ9ijTWn6n9c1Wz4DzmQ")
+BOT_TOKEN = getenv("BOT_TOKEN", "8694198519:AAHkfsd2hG584oC92jM-Ee2PJd2snDy49qM")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
 
-OWNER_ID = int(getenv("OWNER_ID",7892805795))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/anujeditbyak")
+OWNER_ID = int(getenv("OWNER_ID",8730393744))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/akanujak")
