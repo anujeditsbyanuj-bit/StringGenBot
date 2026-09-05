b4074f6d251b8361aea7a5fcbaacb388b3932639
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-09-05 09:12:51 +0530

    Update config.py
    Signed-off-by: anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv("BOT_TOKEN", "8694198519:AAHkfsd2hG584oC92jM-Ee2PJd2snDy49qM")
+BOT_TOKEN = getenv("BOT_TOKEN", "8676646717:AAGfnpz2tjYoTqrJMi5Dv0yX33_ZJGqbKfc")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
 
 OWNER_ID = int(getenv("OWNER_ID",8931907813))
