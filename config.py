d4ad35c1ad45c352b4aee9c373a6ea612fa16e36
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-09-05 09:10:41 +0530

    Update config.py
    Signed-off-by: anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "8694198519:AAHkfsd2hG584oC92jM-Ee2PJd2snDy49qM"
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
 
 OWNER_ID = int(getenv("OWNER_ID",8730393744))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/akanujak")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/anujedits97")
